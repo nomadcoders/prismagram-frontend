bef9e556083874a31530d89353f6265dbb44a2eb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-07 19:24:35 +0900

    #5.0 Auth Route UI part One

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,23 @@
-export default () => "Auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  ${props => props.theme.whiteBox}
+`;
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+
+  return (
+    <Wrapper>
+      {action === "logIn" ? <Box>Log iN</Box> : <Box>Sign Up</Box>}
+    </Wrapper>
+  );
+};
--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -11,8 +11,8 @@ export default {
   darkBlueColor: "#003569",
   boxBorder: "1px solid #e6e6e6",
   borderRadius: "4px",
-  whiteBox: `${BOX_BORDER};
-             ${BORDER_RADIUS};
+  whiteBox: `border:${BOX_BORDER};
+             border-radius:${BORDER_RADIUS};
              background-color:white;
             `
 };
