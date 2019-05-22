42e4ad93a0bb714540091e4ea8cf22d5f597227b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-22 14:21:43 +0900

    Sorry

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -8,7 +8,7 @@ export default createGlobalStyle`
         box-sizing:border-box;
     }
     body {
-        background-color:red;
+        background-color:${props => props.theme.bgColor};
         color:${props => props.theme.blackColor};
         font-size:14px;
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
