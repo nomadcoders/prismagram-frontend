7778415ad901630830164401a919efea6e06d193
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-22 16:34:02 +0900

    Real URL

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://localhost:4000",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "https://prismagram-backend.herokuapp.com",
   clientState: {
     defaults,
     resolvers
